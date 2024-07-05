559adc2c2e66d0116a24f63d1ead1930d9f983d5
Ubuntu <nxn@runningmachine.jmdu0ug1imhetg0qqquesy5uoh.phxx.internal.cloudapp.net>
2024-07-05 04:33:50 +0000

    setup env

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install transformers && \
+pip install diffusers && \
+pip install accelerate && \
+pip install protobuf && \
+pip install matplotlib && \
+pip install torch torchvision torchaudio && \
+pip install einops
